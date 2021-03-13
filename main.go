1c71b4618b781cb941ed437adfae87aaecc43beb
Thuban Arrakis <thuban.arrakis@gmail.com>
2021-03-13 12:04:09 +0300

    init

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+	"io/ioutil"
+)
+
+func main() {
+    mux := http.NewServeMux()
+    mux.HandleFunc("/", clientResponse)
+
+    err := http.ListenAndServe(":8000", mux)
+    log.Fatal(err)
+}
+
+func clientResponse(w http.ResponseWriter, r *http.Request) {
+
+	b, err := ioutil.ReadAll(r.Body)
+    if err != nil {
+		return
+	}
+
+	fmt.Fprintf(w, "Method: %s, Body: %s", r.Method, b)
+}
