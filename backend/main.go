6aeccbd51753e2ff2a1db74329828bb2f960a0ca
Thuban Arrakis <thuban.arrakis@gmail.com>
2021-03-14 14:04:43 +0300

    backend CORS

--- backend/main.go
+++ backend/main.go
@@ -5,18 +5,24 @@ import (
     "log"
     "net/http"
     "io/ioutil"
+    "os"
 )
 
 func main() {
     mux := http.NewServeMux()
     mux.HandleFunc("/", clientResponse)
+    
 
     err := http.ListenAndServe(":8000", mux)
     fmt.Println("starting server...")
     log.Fatal(err)
+    log.SetOutput(os.Stderr)
+    log.SetOutput(os.Stdout)
 }
 
 func clientResponse(w http.ResponseWriter, r *http.Request) {
+
+    w.Header().Set("Access-Control-Allow-Origin", "*")
 
     b, err := ioutil.ReadAll(r.Body)
     if err != nil {
